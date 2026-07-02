115267b0211dc3740e59969f37ab5a17356b9045
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2026-07-02 17:28:22 +0530

    10. spring-data-jpa

--- simple-web-app/src/main/java/com/telusko/simple_web_app/model/Product.java
+++ simple-web-app/src/main/java/com/telusko/simple_web_app/model/Product.java
@@ -1,12 +1,18 @@
 package com.telusko.simple_web_app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
+    @Id
     private int id;
     private String name;
     private double price;
--- simple-web-app/src/main/java/com/telusko/simple_web_app/service/ProductService.java
+++ simple-web-app/src/main/java/com/telusko/simple_web_app/service/ProductService.java
@@ -1,51 +1,48 @@
 package com.telusko.simple_web_app.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.telusko.simple_web_app.model.Product;
+import com.telusko.simple_web_app.repository.ProductRepo;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
+    private final ProductRepo productRepo;
+
     // Dummy data
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(1, "Product 1", 10.0),
-            new Product(2, "Product 2", 20.0),
-            new Product(3, "Product 3", 30.0),
-            new Product(4, "Product 4", 40.0),
-            new Product(5, "Product 5", 50.0)));
+    // List<Product> products = new ArrayList<>(Arrays.asList(
+    // new Product(1, "Product 1", 10.0),
+    // new Product(2, "Product 2", 20.0),
+    // new Product(3, "Product 3", 30.0),
+    // new Product(4, "Product 4", 40.0),
+    // new Product(5, "Product 5", 50.0)));
 
     public List<Product> getAllProducts() {
-        // Logic to retrieve all products from the database or any data source
-        return products;
+
+        return productRepo.findAll();
     }
 
     public Product getProduct(int id) {
-        // Logic to retrieve a product by its ID from the database or any data source
-        return products.stream()
-                .filter(p -> p.getId() == id)
-                .findFirst()
-                .orElse(null);
+        return productRepo.findById(id).orElse(null);
     }
 
     public void addProduct(Product product) {
-        products.add(product);
+        productRepo.save(product);
     }
 
     public void updateProduct(int id, Product product) {
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getId() == id) {
-                products.set(i, product);
-                return;
-            }
+        if (productRepo.existsById(id)) {
+            productRepo.save(product);
         }
     }
 
     public void deleteProduct(int id) {
-        products.removeIf(p -> p.getId() == id);
+        productRepo.deleteById(id);
     }
 }
